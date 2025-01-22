d14ab2b2a7f1bf4ff531051fc58e032ec0640db7
Kwai Liu <[cmdrtomalak@gmail.com](mailto:cmdrtomalak@gmail.com)>
2025-01-22 16:31:09 -0500

    Add Treasury

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,36 @@
 use std::env;
 
 mod utils;
-use crate::utils::fetch::{fetch_mortgage_rates, fetch_unemployment_rates};
+use crate::utils::fetch::{fetch_mortgage_rates, fetch_treasury_rates, fetch_unemployment_rates};
 use crate::utils::webhook::send_to_discord;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let series_id = "MORTGAGE30US";
-    let label = "30-Year Fixed Rate";
-    let table = fetch_mortgage_rates(&series_id, &label).await?;
-
+    let fred_key = env::var("FRED_API_KEY").expect("Need a key");
     let webhook_url = env::var("DISCORD_WEBHOOK_URL").expect("Webhook url not found");
+
+    let table = fetch_mortgage_rates("MORTGAGE30US", "30-Year Fixed Rate", &fred_key).await?;
     send_to_discord(&webhook_url, table).await?;
 
-    let series_id = "MORTGAGE15US";
-    let label = "15-Year Fixed Rate";
-    let table = fetch_mortgage_rates(&series_id, &label).await?;
-
+    let table = fetch_mortgage_rates("MORTGAGE15US", "15-Year Fixed Rate", &fred_key).await?;
     send_to_discord(&webhook_url, table).await?;
 
-    let table = fetch_unemployment_rates().await?;
-
+    let table = fetch_unemployment_rates(&fred_key).await?;
     send_to_discord(&webhook_url, table).await?;
 
+    let table = fetch_treasury_rates("DGS30", "30-Year Treasury Rate", &fred_key).await?;
+    send_to_discord(&webhook_url, table).await?;
+
+    let table = fetch_treasury_rates("DGS20", "20-Year Treasury Rate", &fred_key).await?;
+    send_to_discord(&webhook_url, table).await?;
+
+    let table = fetch_treasury_rates("DGS10", "10-Year Treasury Rate", &fred_key).await?;
+    send_to_discord(&webhook_url, table).await?;
+
+    let table = fetch_treasury_rates("DGS2", "2-Year Treasury Rate", &fred_key).await?;
+    send_to_discord(&webhook_url, table).await?;
+
+    let table = fetch_treasury_rates("DGS6MO", "6-Month Treasury Rate", &fred_key).await?;
+    send_to_discord(&webhook_url, table).await?;
     Ok(())
 }
--- src/utils/fetch.rs
+++ src/utils/fetch.rs
@@ -2,16 +2,15 @@ use chrono::{Duration, Local};
 use fred_rs::client::FredClient;
 use fred_rs::series::observation::{Builder, Frequency, Response, Units};
 use prettytable::{Cell, Row, Table};
-use std::env;
 
 pub async fn fetch_mortgage_rates(
     series_id: &str,
     label: &str,
+    fred_key: &str,
 ) -> Result<Table, Box<dyn std::error::Error>> {
     // Initialize FRED client
     let mut client = FredClient::new()?;
 
-    let fred_key = env::var("FRED_API_KEY").expect("Need a key");
     client.with_key(&fred_key);
 
     let one_month_ago = (Local::now() - Duration::days(30))
@@ -46,11 +45,10 @@ pub async fn fetch_mortgage_rates(
     Ok(table)
 }
 
-pub async fn fetch_unemployment_rates() -> Result<Table, Box<dyn std::error::Error>> {
+pub async fn fetch_unemployment_rates(fred_key: &str) -> Result<Table, Box<dyn std::error::Error>> {
     // Initialize FRED client
     let mut client = FredClient::new()?;
 
-    let fred_key = env::var("FRED_API_KEY").expect("Need a key");
     client.with_key(&fred_key);
 
     let one_month_ago = (Local::now() - Duration::days(30))
@@ -74,6 +72,48 @@ pub async fn fetch_unemployment_rates() -> Result<Table, Box<dyn std::error::Err
             Cell::new(&obs.date),
             Cell::new(&obs.value),
             Cell::new("Percent"),
+        ]));
+    }
+
+    Ok(table)
+}
+
+pub async fn fetch_treasury_rates(
+    series_id: &str,
+    label: &str,
+    fred_key: &str,
+) -> Result<Table, Box<dyn std::error::Error>> {
+    // Initialize FRED client
+    let mut client = FredClient::new()?;
+
+    client.with_key(&fred_key);
+
+    let period = (Local::now() - Duration::days(15))
+        .format("%Y-%m-%d")
+        .to_string();
+
+    let mut builder = Builder::new();
+    builder
+        .units(Units::LIN)
+        .frequency(Frequency::D)
+        .observation_start(&period);
+
+    let resp: Response = client.series_observation(series_id, Some(builder))?;
+
+    let mut table = Table::new();
+    table.add_row(Row::new(vec![
+        Cell::new("Date"),
+        Cell::new(&label),
+        Cell::new("Unit"),
+        Cell::new("Frequency"),
+    ]));
+
+    for obs in resp.observations {
+        table.add_row(Row::new(vec![
+            Cell::new(&obs.date),
+            Cell::new(&obs.value),
+            Cell::new("Percent"),
+            Cell::new("Daily"),
         ]));
     }
 
