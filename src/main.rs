8047c2b8303da8e643b55959b55378b7fbc8c7c7
Kwai Liu <[cmdrtomalak@gmail.com](mailto:cmdrtomalak@gmail.com)>
2025-01-22 15:13:06 -0500

    Generalize mortgage function, add unemployment fetch

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,10 @@ use reqwest::Client;
 use serde_json::json;
 use std::env;
 
-async fn fetch_mortgage_rates() -> Result<Table, Box<dyn std::error::Error>> {
+async fn fetch_mortgage_rates(
+    series_id: &str,
+    label: &str,
+) -> Result<Table, Box<dyn std::error::Error>> {
     // Initialize FRED client
     let mut client = FredClient::new()?;
 
@@ -23,12 +26,12 @@ async fn fetch_mortgage_rates() -> Result<Table, Box<dyn std::error::Error>> {
         .frequency(Frequency::W)
         .observation_start(&one_month_ago);
 
-    let resp: Response = client.series_observation("MORTGAGE30US", Some(builder))?;
+    let resp: Response = client.series_observation(series_id, Some(builder))?;
 
     let mut table = Table::new();
     table.add_row(Row::new(vec![
         Cell::new("Date"),
-        Cell::new("30-Year Fixed Rate"),
+        Cell::new(&label),
         Cell::new("Unit"),
         Cell::new("Frequency"),
     ]));
@@ -45,6 +48,39 @@ async fn fetch_mortgage_rates() -> Result<Table, Box<dyn std::error::Error>> {
     Ok(table)
 }
 
+async fn fetch_unemployment_rates() -> Result<Table, Box<dyn std::error::Error>> {
+    // Initialize FRED client
+    let mut client = FredClient::new()?;
+
+    let fred_key = env::var("FRED_API_KEY").expect("Need a key");
+    client.with_key(&fred_key);
+
+    let one_month_ago = (Local::now() - Duration::days(30))
+        .format("%Y-%m-%d")
+        .to_string();
+
+    let mut builder = Builder::new();
+    builder.units(Units::LIN).observation_start(&one_month_ago);
+
+    let resp: Response = client.series_observation("UNRATE", Some(builder))?;
+
+    let mut table = Table::new();
+    table.add_row(Row::new(vec![
+        Cell::new("Date"),
+        Cell::new("Unemployment Rate"),
+        Cell::new("Unit"),
+    ]));
+
+    for obs in resp.observations {
+        table.add_row(Row::new(vec![
+            Cell::new(&obs.date),
+            Cell::new(&obs.value),
+            Cell::new("Percent"),
+        ]));
+    }
+
+    Ok(table)
+}
 async fn send_to_discord(
     webhook_url: &str,
     table: Table,
@@ -70,9 +106,21 @@ async fn send_to_discord(
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let table = fetch_mortgage_rates().await?;
+    let series_id = "MORTGAGE30US";
+    let label = "30-Year Fixed Rate";
+    let table = fetch_mortgage_rates(&series_id, &label).await?;
 
     let webhook_url = env::var("DISCORD_WEBHOOK_URL").expect("Webhook url not found");
+    send_to_discord(&webhook_url, table).await?;
+
+    let series_id = "MORTGAGE15US";
+    let label = "15-Year Fixed Rate";
+    let table = fetch_mortgage_rates(&series_id, &label).await?;
+
+    send_to_discord(&webhook_url, table).await?;
+
+    let table = fetch_unemployment_rates().await?;
+
     send_to_discord(&webhook_url, table).await?;
 
     Ok(())
